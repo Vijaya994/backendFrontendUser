23d3a90c02ec6045932c894624bace5c38819aa1
vijaya <vijayalakshmi.p4545@gmail.com>
2022-04-01 22:27:36 +0530

    commited with backendFrontendUser

---
+++ frontendreactapp/src/components/FooterComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+function FooterComponent(){
+    return(
+        <div>
+            <div class="alert alert-secondary text-center">
+                All Rights are Reserved@ Feuji 
+            </div>
+        </div>
+    )
+}
+export default FooterComponent;
---
+++ frontendreactapp/src/components/HeaderComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HeaderComponent(){
+    return(
+        <div>
+            <div class="alert alert-dark  text-center">
+                Feuji Organization
+            </div>
+        </div>
+    )
+}
+export default HeaderComponent;
